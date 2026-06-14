f7d4086cfa8a96d2afcddfd1900b5d9616868126
Leonardo-capy <leonardo.santos17@estudante.ifms.edu.br>
2026-06-14 03:55:20 -0400

    correcao da correcao

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,8 +1,9 @@
 import axios from 'axios';
 
 const api = axios.create({
+  // Mudamos aqui para garantir que ele use a variável de produção da Vercel
   baseURL: process.env.NODE_ENV === 'production' 
-    ? (process.env.REACT_APP_API_URL || '') // Usa a variável da Vercel se existir
+    ? process.env.REACT_APP_API_URL 
     : 'http://localhost:3000',
   withCredentials: true,
   timeout: 10000
