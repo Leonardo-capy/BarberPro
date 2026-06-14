dfb6208dcf5c0f72ebc272f3a7ed33c19ff0449a
Leonardo-capy <leonardo.santos17@estudante.ifms.edu.br>
2026-06-14 04:03:30 -0400

    link para correcao

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,9 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-  // Mudamos aqui para garantir que ele use a variável de produção da Vercel
   baseURL: process.env.NODE_ENV === 'production' 
-    ? process.env.REACT_APP_API_URL 
+    ? 'https://barbearia-agenda-nova.onrender.com'
     : 'http://localhost:3000',
   withCredentials: true,
   timeout: 10000
